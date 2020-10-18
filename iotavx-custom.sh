32954e08b0db51e47b072793e5c8df5ed64c4b4b
Luka Krmpotić <luka.krmpotic@cosylab.com>
2020-10-18 23:19:36 +0200

    expand iotavx-custom.sh

--- iotavx-custom.sh
+++ iotavx-custom.sh
@@ -1,24 +1,60 @@
 #!/bin/bash
+dmenu_opts="-l 10 -m 0 -fn 'Sans:pixelsize=50'"
+f_ff="$(mktemp)" # add f-flag file
 
-source  etc/button_codes.sh
+trap "rm -f $f_ff; echo cleaning up... " 1 2 15
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # necessary?
+source $DIR/etc/button_codes.sh
+
+rm -f $f_ff
 while read cmd;
 do
+	if [ -a $f_ff ]; then
+		echo "f flag ON"
+		case $cmd in
+		${button["folder+"]})    xdotool key Home ;;
+		${button["folder-"]})    xdotool key End ;;
+		${button["next"]})       xdotool key Down ;;
+		${button["previous"]})   xdotool key Up ;;
+		${button["play-pause"]}) xdotool key Enter ;;
+		${button["eject"]})      xdotool key Escape ;;
+		*) ;;
+		esac
+		continue
+	fi
+
 	case $cmd in
-	${button["play-pause"]})
+	${button["play-pause"]}) # play/pause
 		playerctl play-pause
 		;;
-	${button["mode"]})
-		dbdb.sh -l 10 -fn "Sans:pixelsize=50"
+	${button["next"]})       # next
+		playerctl next
 		;;
-	${button["shuffle"]})
+	${button["previous"]})   # previous
+		playerctl previous
+		;;
+	${button["stop"]})       # stop
+		playerctl stop
+		;;
+	${button["shuffle"]})    # shuffle on/off
 		playerctl shuffle | grep On && playerctl shuffle Off || playerctl shuffle On
 		;;
-	${button["eject"]})
-		etc/switch-audio.sh
+	${button["repeat"]})       # loop none/playlist
+		playerctl loop | grep None && playerctl loop Playlist || playerctl loop None
+		;;
+	${button["eject"]})      # switch audio output
+		$DIR/etc/switch-audio.sh
+		;;
+	${button["folder+"]})    # xdg-open with directory-browser
+		touch $f_ff
+		{ file=$($DIR/etc/dmenu-sel.sh $dmenu_opts) && \
+			xdg-open $file; rm $f_ff; } &
 		;;
 	*)
 		echo ?? $cmd
 		;;
 	esac
 done < $1
+
+rm -f $f_ff
