dd860e1f6fbf89baa666b61dc1b1f6405668194d
Luka Krmpotić <luka.krmpotic@cosylab.com>
2020-10-19 08:12:30 +0200

    make send_keys callable from anywhere

--- etc/send_keys.sh
+++ etc/send_keys.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-source etc/button_codes.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $DIR/button_codes.sh
 
 for var in $@
 do
-	[[ -n "${button[$var]}" ]] && var=${button["$var"]}
+	[[ -n "${button["$var"]}" ]] && var=${button["$var"]}
 	echo $var > /tmp/irdata.fifo
 	sleep 0.5
 done
